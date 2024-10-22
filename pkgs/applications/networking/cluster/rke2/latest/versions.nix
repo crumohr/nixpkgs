82e13da05303445a3a6b7e8d765004cd7ad671d4
Christoffer Rumohr <c_rumohr@taures.de>
2024-10-22 09:17:56 +0200

    rke2_latest: 1.31.1+rke2r1 -> 1.31.1+rke2r2

--- pkgs/applications/networking/cluster/rke2/latest/versions.nix
+++ pkgs/applications/networking/cluster/rke2/latest/versions.nix
@@ -1,14 +1,14 @@
 {
-  rke2Version = "1.31.1+rke2r1";
-  rke2Commit = "909d20d6a28cd7656b7177190f06f69f57927613";
-  rke2TarballHash = "sha256-9ZryOX6QMNpjDtsOXLOVNPjCc6AMAa+XDLOn1EpyCcg=";
-  rke2VendorHash = "sha256-7nWbWi4oJTOWZ5iZr9ptECDJJakPg4qZ7hW+tU7LBsI=";
+  rke2Version = "1.31.1+rke2r2";
+  rke2Commit = "02fbb2aef3f6876b25d07d6c7105694bc21194f5";
+  rke2TarballHash = "sha256-hXfZ7MnsYac+s9z/dxSo17/N8kgY9LmCvHQB1S0iPdM=";
+  rke2VendorHash = "sha256-tWGkO+CDaMCWHlya22G7lhoFAWyJuKGWR4XYRPBHR/U=";
   k8sVersion = "v1.31.1";
-  k8sImageTag = "v1.31.1-rke2r1-build20240912";
+  k8sImageTag = "v1.31.1-rke2r2-build20241014";
   etcdVersion = "v3.5.13-k3s1-build20240910";
   pauseVersion = "3.6";
-  ccmVersion = "v1.31.0-build20240910";
-  dockerizedVersion = "v1.31.1-rke2r1";
+  ccmVersion = "v1.31.2-0.20241016053446-0955fa330f90-build20241016";
+  dockerizedVersion = "v1.31.1-rke2r2";
   golangVersion = "go1.22.6";
   eol = "2025-10-28";
 }
